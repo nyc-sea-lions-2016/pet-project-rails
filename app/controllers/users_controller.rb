12cde2187ae9fbdc122e844f14b74c64dd0d6ac0
FJ Collins Jr <fcollins@live.com>
2016-04-21 18:11:30 -0400

    add users controller#show - add simple current user method for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+
+
   def show
     # find favorites with user_id
     # find pets for those favorites
     # set pets to array
-    @favorites = Favorite.all
+    # for when we have current_user method
+    @user = User.includes(:pets).find(current_user)
 
   end
 end
