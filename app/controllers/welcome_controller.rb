91c57dae571b5c15cb45900b21c2549c5726d8ec
Alyssa <aransbury@gmail.com>
2016-04-21 16:52:19 -0400

    Add api request to find pet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-    #@potential_pet =
-    #ajax call to pet finder api
+    location = '10005'
+    key = ENV['PET_FINDER_KEY']
+    request_url = 'http://api.petfinder.com/pet.getRandom?key=' + key + '&location=' + location + '&output=full&format=json'
+    @potential_pet = HTTParty.get(request_url)
+    binding.pry
   end
 
 end
