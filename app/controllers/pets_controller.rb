8257465fd44d7c16d56c4a891d377ad93a1e0f0e
Alyssa <aransbury@gmail.com>
2016-04-22 23:08:34 -0400

    Add methods to delete favorites and images for pets that are deleted from db

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,9 @@
 class PetsController < ApplicationController
 
 	def create
+		binding.pry
 		@pet = Pet.new(pet_params)
+		@photo = Photo.new({url: params[:url], pet_id: @pet.id})
 		if @pet.save
 			favorite = Favorite.create(pet_id: @pet.id, user_id: 1)
 		end
@@ -14,12 +16,24 @@ class PetsController < ApplicationController
 	def delete
 		@pet = Pet.find(params[:id])
 		@pet.delete
+		delete_favorites(params[:id], 1)
+		delete_photos(params[:id])
 	end
 
 	private
 
 	def pet_params
 		params.require(:pet).permit(:petfinder_id, :name, :animal, :description, :age, :size, :gender, :breed, :altered, :shots, :special_needs, :contact_city, :contact_zip, :contact_state, :contact_name, :contact_email, :contact_phone)
+	end
+
+	def delete_favorites(pet_id, user_id)
+		Favorite.find_by(pet_id: pet_id, user_id: user_id).destroy
+	end
+
+	def delete_photos(pet_id)
+		unless Favorite.find_by(pet_id: pet_id)
+			Photo.where(pet_id: pet_id).destroy_all
+		end
 	end
 
 end
