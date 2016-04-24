32994388db486cc6f4b55452f74bc4f15d38d7a7
Alyssa <aransbury@gmail.com>
2016-04-24 10:37:51 -0400

    Convert api response data into list of shelter objects

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,16 +1,28 @@
 class SheltersController < ApplicationController
 
+  def index
+    shelters_list = get_shelters_request
+
+    @shelters = []
+    shelters_list["petfinder"]["shelters"]["shelter"].each do |shelter|
+      name = shelter["name"]["$t"]
+      longitude = shelter["longitude"]["$t"]
+      latitude = shelter["latitude"]["$t"]
+      @shelters << Shelter.new({name: name, longitude: longitude, latitude: latitude})
+    end
+    @shelters
+  end
+
+  private
+
   KEY = ENV['PET_FINDER_KEY']
   DEFAULT_LOCATION = '10005'
   OUTPUT = 'full'
   FORMAT = 'json'
 
-  def index
+  def get_shelters_request
     request_url = 'http://api.petfinder.com/shelter.find?key=' + KEY + '&location=' + DEFAULT_LOCATION + '&output=' + OUTPUT + '&format=' + FORMAT
-    response = HTTParty.get(request_url)
-    binding.pry
-    response.each do |shelter|
-
+    HTTParty.get(request_url)
   end
 
 end
