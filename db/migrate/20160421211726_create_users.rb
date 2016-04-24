163b374e6f34c924e0aaf466679c7d6becaa1a4c
Alyssa <aransbury@gmail.com>
2016-04-24 14:13:02 -0400

    Add preferred location and preferred search radius to user migration

--- db/migrate/20160421211726_create_users.rb
+++ db/migrate/20160421211726_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false
-      
+      t.string :preferred_location
+      t.integer :preferred_search_radius
+
       t.timestamps null: false
     end
   end
