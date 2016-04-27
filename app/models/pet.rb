434b149119f77e128f8c9134309ddca1e2ee1f02
Alyssa <aransbury@gmail.com>
2016-04-27 15:04:12 -0400

    Add method to clean up name of animal before passing to client

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,9 @@ class Pet < ActiveRecord::Base
 	has_many :favorites
 	has_many :photos
 	has_many :users, through: :favorites
+
 	after_initialize :init
+	after_initialize :remove_extra_characters_from_name
 
 	validates :petfinder_id, :name, :animal, :description, :age, :size, :gender, :breed, :contact_city, :contact_zip, :contact_state, :contact_address, :contact_email, :contact_phone, { presence: true }
 
@@ -19,15 +21,24 @@ class Pet < ActiveRecord::Base
 		self.contact_state ||= "No contact state on record"
 		self.contact_zip ||= "No contact zip code on record"
 		self.contact_address ||= "No contact address on record"
-		self.contact_email ||= "No contact email address on record"
-		self.contact_phone ||= "No contact phone number on record"
+		self.contact_email ||= (find_email(self.description) || "No contact email address on record")
+		self.contact_phone ||= (find_phone_number(self.description) || "No contact phone number on record")
 	end
 
-	def find_phone_number(description)
-		description.match(/\d{3}-\d{3}-\d{4}/).to_s
+	private
+
+	def remove_extra_characters_from_name
+		self.name = self.name.gsub(/[^a-zA-z\s].+$/, '')
 	end
 
-	def find_email(description)
-		description.match(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/).to_s
+	def find_phone_number(string)
+		phone_number = string.match(/\d{3}-\d{3}-\d{4}/).to_s
+		phone_number == '' ? false : phone_number
 	end
+
+	def find_email(string)
+		email = string.match(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/).to_s
+		email == '' ? false : email
+	end
+
 end
