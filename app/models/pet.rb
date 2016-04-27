4fd255914dfea8f7c1c5fd23cab91c7dc1c1981e
Alyssa <aransbury@gmail.com>
2016-04-27 12:00:58 -0400

    Add methods to pull email address and phone number from description text

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,7 +6,6 @@ class Pet < ActiveRecord::Base
 
 	validates :petfinder_id, :name, :animal, :description, :age, :size, :gender, :breed, :contact_city, :contact_zip, :contact_state, :contact_address, :contact_email, :contact_phone, { presence: true }
 
-
 # init validates everything but petfinder_id & animal(type) & images
 	def init
 		self.name ||= "Please give me a name and a home!"
@@ -20,11 +19,15 @@ class Pet < ActiveRecord::Base
 		self.contact_state ||= "No contact state on record"
 		self.contact_zip ||= "No contact zip code on record"
 		self.contact_address ||= "No contact address on record"
-		self.contact_email ||= "No contact email address on record"
-		self.contact_phone ||= "No contact phone number on record"
+		self.contact_email ||= (find_email(self.description) || "No contact email address on record")
+		self.contact_phone ||= (find_phone_number(self.description) || "No contact phone number on record")
 	end
 
 	def find_phone_number(description)
-		
+		description.match(/\d{3}-\d{3}-\d{4}/).to_s
+	end
+
+	def find_email(description)
+		description.match(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/).to_s
 	end
 end
